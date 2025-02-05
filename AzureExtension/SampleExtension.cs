2bfe965e5648f249995215fff5f27ba1efee6a60
Felipe da Conceicao Guimaraes <felipeda@microsoft.com>
2025-02-04 17:25:04 -0800

    Adding stuff

---
+++ AzureExtension/SampleExtension.cs
@@ -0,0 +1,43 @@
+// Copyright (c) Microsoft Corporation
+// The Microsoft Corporation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Runtime.InteropServices;
+using System.Threading;
+using Microsoft.CmdPal.Extensions;
+
+namespace AzureExtension;
+
+[ComVisible(true)]
+[Guid("23c9363b-1ade-4017-afa7-f57f0351bca1")]
+[ComDefaultInterface(typeof(IExtension))]
+public sealed partial class SampleExtension : IExtension, IDisposable
+{
+    private readonly ManualResetEvent _extensionDisposedEvent;
+
+    private readonly AzureExtensionActionsProvider _provider = new();
+
+    public SampleExtension(ManualResetEvent extensionDisposedEvent)
+    {
+        this._extensionDisposedEvent = extensionDisposedEvent;
+    }
+
+    public object GetProvider(ProviderType providerType)
+    {
+        switch (providerType)
+        {
+            case ProviderType.Commands:
+                return _provider;
+            default:
+#pragma warning disable CS8603 // Possible null reference return.
+                return null;
+#pragma warning restore CS8603 // Possible null reference return.
+        }
+    }
+
+    public void Dispose()
+    {
+        this._extensionDisposedEvent.Set();
+    }
+}
