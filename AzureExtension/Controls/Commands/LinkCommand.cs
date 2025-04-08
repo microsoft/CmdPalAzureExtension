becfa520336e434fb4acfcfd27a8f4b76711bd72
Lauren Ciha <laurenciha@microsoft.com>
2025-04-08 13:39:59 -0700

    Add LinkCommand

---
+++ AzureExtension/Controls/Commands/LinkCommand.cs
@@ -0,0 +1,27 @@
+// Copyright (c) Microsoft Corporation
+// The Microsoft Corporation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Diagnostics;
+using AzureExtension.Helpers;
+using Microsoft.CommandPalette.Extensions.Toolkit;
+
+namespace AzureExtension.Controls.Commands;
+
+internal sealed partial class LinkCommand : InvokableCommand
+{
+    private readonly string _url;
+
+    internal LinkCommand(string url, IResources resources)
+    {
+        Name = resources.GetResource("Commands_Open_Link");
+        Icon = new IconInfo("\uE8A7");
+        _url = url;
+    }
+
+    public override CommandResult Invoke()
+    {
+        Process.Start(new ProcessStartInfo(_url) { UseShellExecute = true });
+        return CommandResult.KeepOpen();
+    }
+}
