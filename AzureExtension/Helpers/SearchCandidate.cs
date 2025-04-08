48f3f9a173753dda953a7ce38e6fd2335692b8b3
Lauren Ciha <laurenciha@microsoft.com>
2025-04-08 14:12:42 -0700

    Cleanup: remove AzureUri from SearchCandidate

--- AzureExtension/Helpers/SearchCandidate.cs
+++ AzureExtension/Helpers/SearchCandidate.cs
@@ -2,7 +2,6 @@
 // The Microsoft Corporation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-using AzureExtension.Client;
 using AzureExtension.Controls;
 
 namespace AzureExtension.Helpers;
@@ -15,8 +14,6 @@ public class SearchCandidate : ISearch
 
     public bool IsTopLevel { get; set; }
 
-    public AzureUri? Uri { get; set; }
-
     public SearchCandidate()
     {
     }
@@ -27,11 +24,10 @@ public class SearchCandidate : ISearch
         SearchString = searchString;
     }
 
-    public SearchCandidate(string name, string searchString, bool isTopLevel, AzureUri? azureUri)
+    public SearchCandidate(string name, string searchString, bool isTopLevel)
     {
         Name = name;
         SearchString = searchString;
         IsTopLevel = isTopLevel;
-        Uri = azureUri;
     }
 }
