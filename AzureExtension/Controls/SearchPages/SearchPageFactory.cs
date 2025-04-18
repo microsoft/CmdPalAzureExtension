3b36b85fe19c135c16f5422eb845b035cb04f2bc
Lauren Ciha <laurenciha@microsoft.com>
2025-04-18 14:09:37 -0700

    First iteration on SearchPageFactory: add CreatePageForSearch and CreateItemForSearch

--- AzureExtension/Controls/SearchPages/SearchPageFactory.cs
+++ AzureExtension/Controls/SearchPages/SearchPageFactory.cs
@@ -2,6 +2,9 @@
 // The Microsoft Corporation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using AzureExtension.Controls.Commands;
+using AzureExtension.DataManager;
+using AzureExtension.Helpers;
 using Microsoft.CommandPalette.Extensions;
 using Microsoft.CommandPalette.Extensions.Toolkit;
 
@@ -9,8 +12,41 @@ namespace AzureExtension.Controls.Pages;
 
 public class SearchPageFactory : ISearchPageFactory
 {
+    private readonly IResources _resources;
+
+    private readonly IDataProvider _dataProvider;
+
+    public SearchPageFactory(IResources resources, IDataProvider dataProvider)
+    {
+        _resources = resources;
+        _dataProvider = dataProvider;
+    }
+
+    public ListPage CreatePageForSearch(IAzureSearch search)
+    {
+        return search.Type switch
+        {
+            AzureSearchType.Query => new WorkItemsSearchPage((IQuery)search, _resources, _dataProvider),
+            AzureSearchType.PullRequestSearch => new PullRequestSearchPage((IPullRequestSearch)search, _resources, _dataProvider),
+            _ => throw new NotImplementedException($"No page for search type {search.Type}"),
+        };
+    }
+
     public IListItem CreateItemForSearch(IAzureSearch search)
     {
-        return new ListItem(new NoOpCommand());
+        return new ListItem(CreatePageForSearch(search))
+        {
+            Title = search.Name,
+            Subtitle = search.Url,
+            Icon = new IconInfo(AzureIcon.IconDictionary["logo"]),
+            MoreCommands = new CommandContextItem[]
+            {
+                new(new LinkCommand(search.Url, _resources))
+                {
+                    Title = _resources.GetResource("Pages_Saved_Searches_Open"),
+                    Icon = new IconInfo(AzureIcon.IconDictionary["open"]),
+                },
+            },
+        };
     }
 }
