3678ebfdd33fc9c9d44e3080991eb736c514f39b
Lauren Ciha <laurenciha@microsoft.com>
2025-04-18 13:55:31 -0700

    Introduce SearchPageFactory

---
+++ AzureExtension/Controls/SearchPages/SearchPageFactory.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (c) Microsoft Corporation
+// The Microsoft Corporation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using Microsoft.CommandPalette.Extensions;
+using Microsoft.CommandPalette.Extensions.Toolkit;
+
+namespace AzureExtension.Controls.Pages;
+
+public class SearchPageFactory : ISearchPageFactory
+{
+    public IListItem CreateItemForSearch(IAzureSearch search)
+    {
+        return new ListItem(new NoOpCommand());
+    }
+}
