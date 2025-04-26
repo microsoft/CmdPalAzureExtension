7ff2ee459f36d3d3967697030cedf182241a7953
guimafelipe <guima.felipec@gmail.com>
2025-04-25 19:30:09 -0700

    Adding pull request search flow test

--- AzureExtension.Test/DataManager/DataManagerTests.cs
+++ AzureExtension.Test/DataManager/DataManagerTests.cs
@@ -10,11 +10,15 @@ using AzureExtension.DataManager;
 using AzureExtension.DataModel;
 using Microsoft.Identity.Client;
 using Microsoft.TeamFoundation.Core.WebApi;
+using Microsoft.TeamFoundation.SourceControl.WebApi;
 using Microsoft.TeamFoundation.WorkItemTracking.WebApi.Models;
 using Microsoft.VisualStudio.Services.Profile;
+using Microsoft.VisualStudio.Services.WebApi;
 using Moq;
+using PullRequestSearch = AzureExtension.DataModel.PullRequestSearch;
 using Query = AzureExtension.DataModel.Query;
 using TFModels = Microsoft.TeamFoundation.WorkItemTracking.WebApi.Models;
+using WorkItem = AzureExtension.DataModel.WorkItem;
 
 namespace AzureExtension.Test.DataManager;
 
@@ -143,7 +147,80 @@ public class DataManagerTests
         Assert.AreEqual("Test Query", dsQuery.Name);
         Assert.AreEqual("TestUsername", dsQuery.Username);
         Assert.AreEqual("Test Project", dsQuery.Project.Name);
+        Assert.AreEqual("Test Work Item", WorkItem.GetForQuery(dataStore, dsQuery).First().SystemTitle);
 
+        CleanUpDataStore(dataStore);
+    }
+
+    [TestMethod]
+    public async Task TestPullRequestSearchUpdateFlow()
+    {
+        var dataStore = GetTestDataStore();
+        var mockAccountProvider = new Mock<IAccountProvider>();
+        var mockLiveDataProvider = new Mock<IAzureLiveDataProvider>();
+        var mockAuthProvider = new Mock<IAuthorizedEntityIdProvider>();
+        var pullRequestSearchManager = new AzureDataPullRequestSearchManager(dataStore, mockAccountProvider.Object, mockLiveDataProvider.Object, mockAuthProvider.Object);
+
+        var stubAccount = new Mock<IAccount>();
+        stubAccount.SetupGet(a => a.Username).Returns("TestUsername");
+
+        mockAccountProvider.Setup(a => a.GetDefaultAccount()).Returns(stubAccount.Object);
+
+        mockAuthProvider.Setup(a => a.GetAuthorizedEntityId(It.IsAny<Uri>(), It.IsAny<IAccount>()))
+            .Returns(Guid.NewGuid());
+
+        mockLiveDataProvider.Setup(p => p.GetTeamProject(It.IsAny<Uri>(), It.IsAny<string>()))
+            .ReturnsAsync(new TeamProject
+            {
+                Id = Guid.NewGuid(),
+                Name = "Test Project",
+                Url = "https://dev.azure.com/Org/Project",
+            });
+
+        mockLiveDataProvider.Setup(p => p.GetRepositoryAsync(It.IsAny<Uri>(), It.IsAny<string>(), It.IsAny<string>(), It.IsAny<CancellationToken>()))
+            .ReturnsAsync(new GitRepository
+            {
+                Id = Guid.NewGuid(),
+                Name = "Test Repository",
+                WebUrl = "https://dev.azure.com/Org/Project/_apis/git/repositories/TestRepository",
+                ProjectReference = new TeamProjectReference
+                {
+                    Visibility = ProjectVisibility.Organization,
+                },
+            });
+
+        mockLiveDataProvider.Setup(p => p.GetPullRequestsAsync(It.IsAny<Uri>(), It.IsAny<string>(), It.IsAny<Guid>(), It.IsAny<GitPullRequestSearchCriteria>(), It.IsAny<CancellationToken>()))
+            .ReturnsAsync(
+            [
+                new GitPullRequest
+                {
+                    PullRequestId = 1,
+                    Title = "Test Pull Request",
+                    Url = "https://dev.azure.com/Org/Project/_apis/git/repositories/TestRepository/pullRequests/1",
+                    Status = PullRequestStatus.Active,
+                    TargetRefName = "refs/heads/main",
+                    CreationDate = DateTime.UtcNow,
+                    CreatedBy = new IdentityRef
+                    {
+                        DisplayName = "Test User",
+                        Url = "https://dev.azure.com/Org/_apis/identities/12345678-1234-1234-1234-1234567890ab",
+                    },
+                },
+            ]);
+
+        var testPullRequestSearch = new Mock<IPullRequestSearch>();
+        testPullRequestSearch.SetupGet(q => q.Url).Returns("https://dev.azure.com/organization/project/_pullRequests/12345678-1234-1234-1234-1234567890ab");
+        testPullRequestSearch.SetupGet(q => q.Name).Returns("Test Pull Request Search");
+        testPullRequestSearch.SetupGet(q => q.View).Returns("All");
+
+        await pullRequestSearchManager.UpdatePullRequestsAsync(testPullRequestSearch.Object, CancellationToken.None);
+
+        var dsPullRequestSearch = PullRequestSearch.Get(dataStore, 1);
+        Assert.IsNotNull(dsPullRequestSearch);
+        Assert.AreEqual("TestUsername", dsPullRequestSearch.Username);
+        Assert.AreEqual("Test Project", dsPullRequestSearch.Project.Name);
+        Assert.AreEqual("Test Repository", dsPullRequestSearch.Repository.Name);
+        Assert.AreEqual("Test Pull Request", PullRequest.GetForPullRequestSearch(dataStore, dsPullRequestSearch).First().Title);
         CleanUpDataStore(dataStore);
     }
 }
