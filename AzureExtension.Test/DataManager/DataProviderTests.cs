78f81d1a8530018995de410ef826030c57077bc3
guimafelipe <guima.felipec@gmail.com>
2025-05-09 20:01:35 -0700

    Fixing tests

--- AzureExtension.Test/DataManager/DataProviderTests.cs
+++ AzureExtension.Test/DataManager/DataProviderTests.cs
@@ -19,11 +19,12 @@ public class DataProviderTests
     [TestMethod]
     public void DataProviderCreate()
     {
+        var stubContentDataDictionary = new Dictionary<Type, IContentDataProvider>();
+        var stubSearchDataDictionary = new Dictionary<Type, ISearchDataProvider>();
         var dataProvider = new LiveDataProvider(
             new Mock<ICacheManager>().Object,
-            new Mock<IDataProvider<IQuery, Query, WorkItem>>().Object,
-            new Mock<IDataProvider<IPullRequestSearch, PullRequestSearch, PullRequest>>().Object,
-            new Mock<IDataProvider<IPipelineDefinitionSearch, Definition, Build>>().Object);
+            stubContentDataDictionary,
+            stubSearchDataDictionary);
 
         Assert.IsNotNull(dataProvider);
     }
@@ -32,39 +33,55 @@ public class DataProviderTests
     public async Task DataProviderGetWorkItemsFromFreshQuery()
     {
         var mockCacheManager = new Mock<ICacheManager>();
-        var mockQueryProvider = new Mock<IDataProvider<IQuery, Query, WorkItem>>();
-        var stubPullRequestSearchProvider = new Mock<IDataProvider<IPullRequestSearch, PullRequestSearch, PullRequest>>();
-        var stubPipelineProvider = new Mock<IDataProvider<IPipelineDefinitionSearch, Definition, Build>>();
+        var mockQueryProvider = new Mock<ISearchDataProvider>();
+        var mockQueryContentProvider = new Mock<IContentDataProvider>();
+        var stubContentDataDictionary = new Dictionary<Type, IContentDataProvider>
+        {
+            { typeof(IQuery), mockQueryContentProvider.Object },
+        };
+
+        var stubSearchDataDictionary = new Dictionary<Type, ISearchDataProvider>
+        {
+            { typeof(IQuery), mockQueryProvider.Object },
+        };
+
         var dataProvider = new LiveDataProvider(
             mockCacheManager.Object,
-            mockQueryProvider.Object,
-            stubPullRequestSearchProvider.Object,
-            stubPipelineProvider.Object);
+            stubContentDataDictionary,
+            stubSearchDataDictionary);
 
         var stubQuery = new Mock<IQuery>();
 
-        var getWorkItemsTask = dataProvider.GetWorkItems(stubQuery.Object);
+        var getWorkItemsTask = dataProvider.GetContentData<IWorkItem>(stubQuery.Object);
 
         mockCacheManager.Verify(m => m.RequestRefresh(It.IsAny<DataUpdateParameters>()), Times.Once);
-        mockQueryProvider.Verify(m => m.GetDataObjects(It.IsAny<IQuery>()), Times.Never);
+        mockQueryContentProvider.Verify(m => m.GetDataObjects(It.IsAny<IQuery>()), Times.Never);
         mockCacheManager.Raise(m => m.OnUpdate += null, new CacheManagerUpdateEventArgs(CacheManagerUpdateKind.Updated));
         await getWorkItemsTask;
 
-        mockQueryProvider.Verify(m => m.GetDataObjects(It.IsAny<IQuery>()), Times.Once);
+        mockQueryContentProvider.Verify(m => m.GetDataObjects(It.IsAny<IQuery>()), Times.Once);
     }
 
     [TestMethod]
     public async Task DataProviderGetWorkItemsFromCachedQuery()
     {
         var mockCacheManager = new Mock<ICacheManager>();
-        var mockQueryProvider = new Mock<IDataProvider<IQuery, Query, WorkItem>>();
-        var stubPullRequestSearchProvider = new Mock<IDataProvider<IPullRequestSearch, PullRequestSearch, PullRequest>>();
-        var stubPipelineProvider = new Mock<IDataProvider<IPipelineDefinitionSearch, Definition, Build>>();
+        var mockQueryProvider = new Mock<ISearchDataProvider>();
+        var mockQueryContentProvider = new Mock<IContentDataProvider>();
+        var stubContentDataDictionary = new Dictionary<Type, IContentDataProvider>
+        {
+            { typeof(IQuery), mockQueryContentProvider.Object },
+        };
+
+        var stubSearchDataDictionary = new Dictionary<Type, ISearchDataProvider>
+        {
+            { typeof(IQuery), mockQueryProvider.Object },
+        };
+
         var dataProvider = new LiveDataProvider(
             mockCacheManager.Object,
-            mockQueryProvider.Object,
-            stubPullRequestSearchProvider.Object,
-            stubPipelineProvider.Object);
+            stubContentDataDictionary,
+            stubSearchDataDictionary);
 
         var stubQuery = new Mock<IQuery>();
 
@@ -74,148 +91,39 @@ public class DataProviderTests
             .Setup(m => m.GetDataForSearch(It.IsAny<IQuery>()))
             .Returns(dsQuery);
 
-        var getWorkItemsTask = dataProvider.GetWorkItems(stubQuery.Object);
+        var getWorkItemsTask = dataProvider.GetContentData<IWorkItem>(stubQuery.Object);
 
         mockCacheManager.Verify(m => m.RequestRefresh(It.IsAny<DataUpdateParameters>()), Times.Once);
-        mockQueryProvider.Verify(m => m.GetDataObjects(It.IsAny<IQuery>()), Times.Once);
+        mockQueryContentProvider.Verify(m => m.GetDataObjects(It.IsAny<IQuery>()), Times.Once);
         mockCacheManager.Raise(m => m.OnUpdate += null, new CacheManagerUpdateEventArgs(CacheManagerUpdateKind.Updated));
-
         await getWorkItemsTask;
 
-        mockQueryProvider.Verify(m => m.GetDataObjects(It.IsAny<IQuery>()), Times.Once);
-    }
-
-    [TestMethod]
-    public async Task DataProviderPullRequestsFromFreshPullRequestSearch()
-    {
-        var mockCacheManager = new Mock<ICacheManager>();
-        var stubQueryProvider = new Mock<IDataProvider<IQuery, Query, WorkItem>>();
-        var mockPullRequestSearchProvider = new Mock<IDataProvider<IPullRequestSearch, PullRequestSearch, PullRequest>>();
-        var stubPipelineProvider = new Mock<IDataProvider<IPipelineDefinitionSearch, Definition, Build>>();
-        var dataProvider = new LiveDataProvider(
-            mockCacheManager.Object,
-            stubQueryProvider.Object,
-            mockPullRequestSearchProvider.Object,
-            stubPipelineProvider.Object);
-
-        var stubPullRequestSearch = new Mock<IPullRequestSearch>();
-
-        var getPullRequestsTask = dataProvider.GetPullRequests(stubPullRequestSearch.Object);
-
-        mockCacheManager.Verify(m => m.RequestRefresh(It.IsAny<DataUpdateParameters>()), Times.Once);
-        mockPullRequestSearchProvider.Verify(m => m.GetDataObjects(It.IsAny<IPullRequestSearch>()), Times.Never);
-        mockCacheManager.Raise(m => m.OnUpdate += null, new CacheManagerUpdateEventArgs(CacheManagerUpdateKind.Updated));
-
-        await getPullRequestsTask;
-
-        mockPullRequestSearchProvider.Verify(m => m.GetDataObjects(It.IsAny<IPullRequestSearch>()), Times.Once);
-    }
-
-    [TestMethod]
-    public async Task DataProviderPullRequestsFromCachedPullRequestSearch()
-    {
-        var mockCacheManager = new Mock<ICacheManager>();
-        var stubQueryProvider = new Mock<IDataProvider<IQuery, Query, WorkItem>>();
-        var mockPullRequestSearchProvider = new Mock<IDataProvider<IPullRequestSearch, PullRequestSearch, PullRequest>>();
-        var stubPipelineProvider = new Mock<IDataProvider<IPipelineDefinitionSearch, Definition, Build>>();
-        var dataProvider = new LiveDataProvider(
-            mockCacheManager.Object,
-            stubQueryProvider.Object,
-            mockPullRequestSearchProvider.Object,
-            stubPipelineProvider.Object);
-
-        var stubPullRequestSearch = new Mock<IPullRequestSearch>();
-
-        var dsPullRequestSearch = new DataModel.PullRequestSearch();
-        mockPullRequestSearchProvider
-            .Setup(m => m.GetDataForSearch(It.IsAny<IPullRequestSearch>()))
-            .Returns(dsPullRequestSearch);
-
-        var getPullRequestsTask = dataProvider.GetPullRequests(stubPullRequestSearch.Object);
-
-        mockCacheManager.Verify(m => m.RequestRefresh(It.IsAny<DataUpdateParameters>()), Times.Once);
-        mockPullRequestSearchProvider.Verify(m => m.GetDataObjects(It.IsAny<IPullRequestSearch>()), Times.Once);
-        mockCacheManager.Raise(m => m.OnUpdate += null, new CacheManagerUpdateEventArgs(CacheManagerUpdateKind.Updated));
-
-        await getPullRequestsTask;
-
-        mockPullRequestSearchProvider.Verify(m => m.GetDataObjects(It.IsAny<IPullRequestSearch>()), Times.Once);
-    }
-
-    [TestMethod]
-    public async Task DataProviderBuildFromFreshDefinition()
-    {
-        var mockCacheManager = new Mock<ICacheManager>();
-        var stubQueryProvider = new Mock<IDataProvider<IQuery, Query, WorkItem>>();
-        var stubPullRequestSearchProvider = new Mock<IDataProvider<IPullRequestSearch, PullRequestSearch, PullRequest>>();
-        var mockPipelineProvider = new Mock<IDataProvider<IPipelineDefinitionSearch, Definition, Build>>();
-        var dataProvider = new LiveDataProvider(
-            mockCacheManager.Object,
-            stubQueryProvider.Object,
-            stubPullRequestSearchProvider.Object,
-            mockPipelineProvider.Object);
-
-        var stubDefinitionSearch = new Mock<IPipelineDefinitionSearch>();
-
-        var getBuildTask = dataProvider.GetBuilds(stubDefinitionSearch.Object);
-
-        mockCacheManager.Verify(m => m.RequestRefresh(It.IsAny<DataUpdateParameters>()), Times.Once);
-        mockPipelineProvider.Verify(m => m.GetDataObjects(It.IsAny<IPipelineDefinitionSearch>()), Times.Never);
-        mockCacheManager.Raise(m => m.OnUpdate += null, new CacheManagerUpdateEventArgs(CacheManagerUpdateKind.Updated));
-
-        await getBuildTask;
-
-        mockPipelineProvider.Verify(m => m.GetDataObjects(It.IsAny<IPipelineDefinitionSearch>()), Times.Once);
-    }
-
-    [TestMethod]
-    public async Task DataProviderBuildFromCachedDefinition()
-    {
-        var mockCacheManager = new Mock<ICacheManager>();
-        var stubQueryProvider = new Mock<IDataProvider<IQuery, Query, WorkItem>>();
-        var stubPullRequestSearchProvider = new Mock<IDataProvider<IPullRequestSearch, PullRequestSearch, PullRequest>>();
-        var mockPipelineProvider = new Mock<IDataProvider<IPipelineDefinitionSearch, Definition, Build>>();
-        var dataProvider = new LiveDataProvider(
-            mockCacheManager.Object,
-            stubQueryProvider.Object,
-            stubPullRequestSearchProvider.Object,
-            mockPipelineProvider.Object);
-
-        var stubDefinitionSearch = new Mock<IPipelineDefinitionSearch>();
-
-        var dsDefinition = new Definition();
-
-        mockPipelineProvider
-            .Setup(m => m.GetDataForSearch(It.IsAny<IPipelineDefinitionSearch>()))
-            .Returns(dsDefinition);
-
-        var getBuildTask = dataProvider.GetBuilds(stubDefinitionSearch.Object);
-
-        mockCacheManager.Verify(m => m.RequestRefresh(It.IsAny<DataUpdateParameters>()), Times.Once);
-        mockPipelineProvider.Verify(m => m.GetDataObjects(It.IsAny<IPipelineDefinitionSearch>()), Times.Once);
-        mockCacheManager.Raise(m => m.OnUpdate += null, new CacheManagerUpdateEventArgs(CacheManagerUpdateKind.Updated));
-
-        await getBuildTask;
-
-        mockPipelineProvider.Verify(m => m.GetDataObjects(It.IsAny<IPipelineDefinitionSearch>()), Times.Once);
+        mockQueryContentProvider.Verify(m => m.GetDataObjects(It.IsAny<IQuery>()), Times.Once);
     }
 
     [TestMethod]
     public async Task DataProviderDefinitionFromFreshDefinition()
     {
         var mockCacheManager = new Mock<ICacheManager>();
-        var stubQueryProvider = new Mock<IDataProvider<IQuery, Query, WorkItem>>();
-        var stubPullRequestSearchProvider = new Mock<IDataProvider<IPullRequestSearch, PullRequestSearch, PullRequest>>();
-        var mockPipelineProvider = new Mock<IDataProvider<IPipelineDefinitionSearch, Definition, Build>>();
+        var mockPipelineProvider = new Mock<ISearchDataProvider>();
+        var mockPipelineContentProvider = new Mock<IContentDataProvider>();
+        var stubContentDataDictionary = new Dictionary<Type, IContentDataProvider>()
+        {
+            { typeof(IPipelineDefinitionSearch), mockPipelineContentProvider.Object },
+        };
+        var stubSearchDataDictionary = new Dictionary<Type, ISearchDataProvider>
+        {
+            { typeof(IPipelineDefinitionSearch), mockPipelineProvider.Object },
+        };
+
         var dataProvider = new LiveDataProvider(
             mockCacheManager.Object,
-            stubQueryProvider.Object,
-            stubPullRequestSearchProvider.Object,
-            mockPipelineProvider.Object);
+            stubContentDataDictionary,
+            stubSearchDataDictionary);
 
         var stubDefinitionSearch = new Mock<IPipelineDefinitionSearch>();
 
-        var getDefinitionTask = dataProvider.GetDefinition(stubDefinitionSearch.Object);
+        var getDefinitionTask = dataProvider.GetSearchData<IDefinition>(stubDefinitionSearch.Object);
 
         mockCacheManager.Verify(m => m.RequestRefresh(It.IsAny<DataUpdateParameters>()), Times.Once);
         mockPipelineProvider.Verify(m => m.GetDataForSearch(It.IsAny<IPipelineDefinitionSearch>()), Times.Once);
