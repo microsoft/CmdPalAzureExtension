e90c74c5926361ee6b213bb0d841d1f5a17081de
guimafelipe <guima.felipec@gmail.com>
2025-05-14 17:06:30 -0700

    Copying telemetry code from github extension

---
+++ AzureExtension/Telemetry/LogLevel.cs
@@ -0,0 +1,41 @@
+﻿// Copyright (c) Microsoft Corporation
+// The Microsoft Corporation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+namespace AzureExtension.Telemetry;
+
+/// <summary>
+/// Telemetry Levels.
+/// These levels are defined by our telemetry system, so it's possible the sampling
+/// could change in the future.
+/// There aren't any convenient enums we can consume, so create our own.
+/// </summary>
+public enum LogLevel
+{
+    /// <summary>
+    /// Local.
+    /// Only log telemetry locally on the machine (similar to an ETW event).
+    /// </summary>
+    Local,
+
+    /// <summary>
+    /// Info.
+    /// Send telemetry from internal and flighted machines, but no external retail machines.
+    /// </summary>
+    Info,
+
+    /// <summary>
+    /// Measure.
+    /// Send telemetry from internal and flighted machines, plus a small, sample % of retail machines.
+    /// Should only be used for telemetry we use to derive measures from.
+    /// </summary>
+    Measure,
+
+    /// <summary>
+    /// Critical.
+    /// Send telemetry from all devices sampled at 100%.
+    /// Should only be used for approved events.
+    /// </summary>
+    Critical,
+}
+
