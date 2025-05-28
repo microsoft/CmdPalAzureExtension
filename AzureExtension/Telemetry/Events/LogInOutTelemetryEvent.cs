a791f6c5a181b247543d1e3a8a878a347e2ffd5a
guimafelipe <guima.felipec@gmail.com>
2025-05-28 15:28:12 -0700

    Adding event data

--- AzureExtension/Telemetry/Events/LogInOutTelemetryEvent.cs
+++ AzureExtension/Telemetry/Events/LogInOutTelemetryEvent.cs
@@ -2,11 +2,13 @@
 // The Microsoft Corporation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using System.Diagnostics.Tracing;
 using Microsoft.Diagnostics.Telemetry;
 using Microsoft.Diagnostics.Telemetry.Internal;
 
 namespace AzureExtension.Telemetry.Events;
 
+[EventData]
 public class LogInOutTelemetryEvent : TelemetryEventBase
 {
     internal override PartA_PrivTags PartA_PrivTags => PrivTags.ProductAndServicePerformance;
